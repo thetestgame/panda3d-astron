ffe97ab1f23ad2635640667aaed466d67fde4f06
thetestgame <nxtlabs13@gmail.com>
2022-02-18 14:53:33 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='Panda3D Astron server support as a Python 3 module. Allows the use of Astron with stock Panda3D',
     long_description=long_description,
     license='MIT',
-    version='1.0.0',
+    version='1.0.1',
     author='Jordan Maxwell',
     maintainer='Jordan Maxwell',
     url='https://github.com/thetestgame/panda3d_astron',
